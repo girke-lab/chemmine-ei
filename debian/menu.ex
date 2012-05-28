b9af697f8e9db1c92ad93696ada77569ea9bf530
khoran <khoran@cs.ucr.edu>
2012-05-28 16:04:33 -0700

    debianized

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(ei):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="ei" command="/usr/bin/ei"
--- src/atompair/R/ChemmineRpp/src/db_build.h
+++
@@ -1,4 +0,0 @@
-#ifndef DB_BUILD_H_
-#define DB_BUILD_H_
-int batch_sdf_parse(const char* sdfile, const char* dbfile);
-#endif /*DB_BUILD_H_*/
